420f0d669c387bfad435f62a38fe21cacd7f9286
znurislamova <znurislamova@moysklad.ru>
2025-04-25 16:35:24 +0300

    added header, main, footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './node_modules/flowbite/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [
     require('flowbite/plugin')
